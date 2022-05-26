cea3ab150f9fe123569660ed68b3ea6a34d41219
Taurien <michel.a.c474@gmail.com>
2022-05-26 03:31:15 -0500

    added replit node and treetrie

--- src/assets/TrieTree.js
+++ src/assets/TrieTree.js
@@ -1,3 +1,123 @@
 import { TrieTreeNode } from "./TrieTreeNode";
 
-export class TrieTree {}
+export class TrieTree {
+    /* */
+    /* TrieTree: Es un árbol de prefijos con múltiples vías que almacena strings de forma eficiente
+    con métodos para insertar un string en el árbol, un método para comprobar si la estructura 
+    contiene una coincidencia para una determinado string y 
+    un método para obtener todas las cadenas que comienzan con un string de prefijo. */
+    constructor() {
+      //¿Cuáles son las caracteristicas del árbol trie?
+      this.root = new TrieTreeNode(null)
+    }
+  
+    findNode(prefix) {
+      /*
+        Regresa un arreglo: [node, depth]
+        1. nodo más profundo en el árbol trie que coincide con el prefijo más largo del string dado 
+        2. la profundidad del nodo.
+  
+        La profundidad devuelta es igual al número de caracteres de prefijo coincidentes.
+        La búsqueda se realiza de forma iterativa con un bucle que comienza desde el nodo raíz.
+      */
+      let current = this.root;
+      for (let char in prefix) {
+        if (current.hasChild(char)) {
+          current = current.child[char];
+        } else {
+          return [undefined]
+        }
+        return
+      }
+    }
+  
+    traverse(node, prefix, visit, n) {
+      /*
+        Recorre el trie tree de forma recursiva.
+        Empezando con el prefijo dado y visita (agrega a la lista) cada nodo con la función visit
+      */
+      if (n === 0) {
+        return visit(prefix);
+      }
+      this.traverse(node, prefix + n, visit, n - 1)
+    }
+  
+    isEmpty() {
+      let node = this.root
+      // Regresa true si el trie tree está vacío (no contiene strings)
+      return Object.keys(node.child).length === 0
+    }
+  
+    contains(str) {
+      
+      if (!str) return
+      else {
+        console.log(str)
+        
+      }
+      // Regresa true si el string se encuentra en el trie tree
+      return
+    }
+  
+    insert(string) {
+      // Agrega un string con todos los caracteres en el trie tree
+      let node = this.root
+  
+      for (const char of string) {
+        // console.log(char)
+        if (node.child[char] == null) node.child[char] = new Node(char)
+        node = node.child[char]
+      }
+  
+      node.terminal = true
+    }
+  
+  
+    complete(prefix) {
+      // Regresa una lista de strings que coincidan con el prefijo
+      let current = this.root;
+      for (let i = 0; i < prefix.lenght; i++) {
+        if (!current.child[prefix[i]]) {
+          return [];
+        }
+        current = current.child[prefix[i]]
+      }
+      let words = [];
+      for (let key in current.child) {
+  
+      }
+    }
+  
+    allTreeStrings() {
+      // Crea la lista de todos los strings almacenados en este trie tree
+      const words = []
+      let node = this.root
+  
+  
+      const search = (node, str) => {
+        for (const entry of Object.entries(node.child)) {
+          let char = entry[0];
+          node = entry[1];
+          if (node.terminal) {
+            words.push(str.concat(char));
+          }
+          search(node, str.concat(char));
+        }
+      };
+      search(node, ""); // first call
+  
+  
+      return words
+    }
+  }
+  
+  const myTree = new TrieTree()
+  console.log(myTree.isEmpty())
+  myTree.insert('hola')
+  myTree.insert('hole')
+  console.log(JSON.stringify(myTree), 'x')
+  console.log()
+  // console.log(myTree, 'y')
+  console.log(myTree.allTreeStrings())
+  console.log(myTree.isEmpty())
+  console.log(myTree.contains('hola'))
--- src/assets/TrieTreeNode.js
+++ src/assets/TrieTreeNode.js
@@ -1,1 +1,36 @@
-export class TrieTreeNode {}
+export class TrieTreeNode {
+    constructor(value) {
+      //Propiedades del Nodo para el árbol trie
+      this.value = value;
+      this.child = {}
+      this.terminal = false;
+    }
+  
+    isTerminal() {
+      //Regresar true si se trata de un nodo terminal
+      return this.terminal;
+    }
+  
+    numChildren() {
+      //Regresa el numero de nodos hijos del nodo actual.
+      return Object.keys(this.child).length;
+    }
+  
+    hasChild(char) {
+      //Regresar true si el nodo contiene un nodo hijo que almacena un caracter dado.
+      return this.child[char] !== undefined;
+    }
+  
+    getChild(char) {
+      //Regresar el nodo hijo si es que almacena un caracter dado
+      return this.child[char];
+    }
+  
+    addChild(char, child_node) {
+      //Agregamos el caracter en el nodo hijo de este nodo
+      //No deberás agregar el nodo si ya existe un nodo hijo con ese caracter
+      if (!this.child[char]) {
+        this.child[char] = child_node;
+      }
+    }
+  }
